bfc983db511024e97110bfc147f40aa08039506f
wmsangel <wmsangel@gmail.com>
2022-10-10 22:34:06 +0600

    Правки

--- js/core.js
+++ js/core.js
@@ -1,9 +1,18 @@
 $(function(){
     // fixed header
+    AOS.init({
+        once: true, // whether animation should happen only once - while scrolling down
+        duration: 900, // values from 0 to 3000, with step 50ms
+
+
+    });
+
+
+
     $nav = $('header');
     $window = $(window);
     $window.scroll(function(){
-      if ($window.scrollTop() > 1) {
+      if ($window.scrollTop() > 80) {
         $('body').addClass('is-header-fixed');
       } else {
         $('body').removeClass('is-header-fixed');
@@ -12,11 +21,11 @@ $(function(){
 
 
     $('.js-goto-link').on('click', function () {
-        $('body').removeClass('mobile-menu-opened')
+        $('body').removeClass('is-mobile-menu-opened')
         var currentLink = $(this).data('pageid');
         $("html, body").animate({
-          scrollTop: $('#'+currentLink).offset().top
-        }, 600);
+          scrollTop: $('#'+currentLink).offset().top - 120
+        }, 1200);
       });
 
 
@@ -32,6 +41,7 @@ $(function(){
     var menu = ['1', '2', '3', '4']
     const pictureSlider = new Swiper('.js-pictures-slide', {
       slidesPerView: 1,
+      loop: true,
       navigation: {
         nextEl: '#pictures .swiper-button-next',
         prevEl: '#pictures .swiper-button-prev',
@@ -50,6 +60,7 @@ $(function(){
     // spec
     const specsSlider = new Swiper('.js-spec-swiper', {
       slidesPerView: 1,
+        loop: true,
       navigation: {
         nextEl: '#spec .swiper-button-next',
         prevEl: '#spec .swiper-button-prev',
